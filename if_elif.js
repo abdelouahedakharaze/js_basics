ebb8427a130355c98637295dbddaa494562101f2
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-11-04 18:56:51 +0100

    if_elif.js

--- if_elif.js
+++
@@ -1,9 +0,0 @@
-let num = 10;
-
-if (num > 0) {
-    console.log("The number is positive.");
-} else if (num < 0) {
-    console.log("The number is negative.");
-} else {
-    console.log("The number is zero.");
-}
