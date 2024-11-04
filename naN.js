41a903acba99e8cc0b9c1f393935006ff705f7c2
abdelouahed akharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2024-11-04 18:28:11 +0100

    naN.js

--- naN.js
+++
@@ -1,7 +0,0 @@
-// NaN not a number
-let a = "hello";
-let b = parseFloat(a);
-console.log(b);
-console.log("// ===== this is confusing too =======");
-console.log(b == NaN); // <== gives False, anything==NaN is false
-console.log(isNaN(b)); // use this instead
